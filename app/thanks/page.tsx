85c248745b02d6c8950a213ef1c93ea8f7ee9404
Guilherme Souza <gui.edu0380@gmail.com>
2025-12-15 10:57:39 -0300

    ajustando responsividade do /thanks

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -6,13 +6,14 @@ export default function Thanks() {
     return (
         <main className={`h-svh flex flex-col justify-center items-center bg-purple-100 px-6 text-center`}>
             <h2 className={`text-4xl lg:text-5xl mb-8 text-gray-900 ${lato.className}}`}>Obrigado por confirmar sua presença!</h2>
-            <p className={`text-lg text-gray-700 max-w-xl mb-6 ${rubik.className}`}>
+            <p className={`text-lg text-gray-700 max-w-3xl mb-6 ${rubik.className}`}>
                 Ficamos felizes em saber que você estará conosco neste dia tão especial.
                 Sua presença significa muito e estamos ansiosos para celebrar juntos. <br />
+
                 Até breve!
             </p>
             <button
-                className={`${rubik.className} text-lg bg-amber-200 shadow-lg py-2 px-4 rounded-xl border-transparent hover:bg-amber-700 hover:text-white transition`}
+                className={`${rubik.className} text-lg bg-amber-200 shadow-lg py-2 px-10 rounded-xl border-transparent sm:text-xl sm:px-20 hover:bg-amber-700 hover:text-white transition`}
                 onClick={() => { window.location.href = 'https://casamento-suelen-andre.vercel.app/' }
                 }>
                 Voltar para o Site!</button>
