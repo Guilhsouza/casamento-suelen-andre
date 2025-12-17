069d803ed373b5b8c4283949d6500778a46283e7
Guilherme Souza <gui.edu0380@gmail.com>
2025-12-17 14:01:00 -0300

    criando openGraph para compartilhar link

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Suelen & André",
   description: "",
+  openGraph: {
+    images: [
+      {
+        url: '/save-the-date.png',
+        width: 1200,
+        height: 630,
+      },
+    ]
+  },
 };
 
 export default function RootLayout({
